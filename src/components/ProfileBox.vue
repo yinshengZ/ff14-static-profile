5f2642a868dce72fb989bdadb159b5d0a6cd74d9
yinshengZ <zys2701999@gmail.com>
2022-07-01 17:16:32 +0100

    initial commit

---
+++ src/components/ProfileBox.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <n-grid x-gap="50" :cols="4">
+            <n-gi>
+                  <n-card  class="profile-card" title="Paladin">
+                   <template #cover>
+                    <img src="../assets/profile-pics/Freya.jpg" alt="Freya">
+                   </template> 
+
+                   Freya Arianrhod
+                    
+                  </n-card>
+            </n-gi>
+
+            <n-gi>
+                <n-card>
+                    Player 2
+                </n-card>
+            </n-gi>
+
+            <n-gi>
+                <n-card>
+                     Player 3
+                </n-card>
+               
+            </n-gi>
+            
+            <n-gi>
+                <n-card>Player 4</n-card>
+                
+            </n-gi>
+
+        </n-grid>
+      
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.profile-card{
+    background-color:transparent;
+    outline:auto;
+}
+</style>
