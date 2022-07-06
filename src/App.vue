344925f1159f00989a902a559b9de316ca061015
YinshengZ <zys2701999@gmail.com>
2022-07-06 18:39:45 +0100

    still playing around with styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
  
  <div>
+  <profile-card>
+    
+  </profile-card>
+
   <profile-box></profile-box>
  </div>
   
@@ -8,16 +12,22 @@
 
 <script>
 import ProfileBox from './components/ProfileBox.vue'
-
+//import ProfileCard from './components/ProfileCard.vue'
 
 export default {
   name: 'App',
   components: {
+    //ProfileCard,
     ProfileBox
   }
 }
 </script>
 
 <style>
+
+body{
+margin:25px;
+background-color:#F6F4F9;
+}
 
 </style>
--- src/components/ProfileBox.vue
+++ src/components/ProfileBox.vue
@@ -1,37 +1,25 @@
 <template>
-    <div>
-        <n-grid x-gap="50" :cols="4">
-            <n-gi>
-                  <n-card  class="profile-card" title="Paladin">
-                   <template #cover>
-                    <img src="../assets/profile-pics/Freya.jpg" alt="Freya">
-                   </template> 
-
-                   Freya Arianrhod
+    <div>        
+         <div class="profile-box">            
+                   
+                <div class="profile-pic">                 
+                        <img src="../assets/profile-pics/freya2.png" alt="Freya">
+                        <div class="character-name">Freya Arianrhod</div>
+                </div> 
+                <div class="description">
+                     <div class="job-icons">
+                        <img src="../assets/job-icons/Paladin.webp" alt="Paladin Icon"> 
+                      
+                     </div>
+                 <div class="profile">
                     
-                  </n-card>
-            </n-gi>
-
-            <n-gi>
-                <n-card>
-                    Player 2
-                </n-card>
-            </n-gi>
-
-            <n-gi>
-                <n-card>
-                     Player 3
-                </n-card>
-               
-            </n-gi>
-            
-            <n-gi>
-                <n-card>Player 4</n-card>
+                    <p>Freya Arianrhod</p>
+                 </div>    
                 
-            </n-gi>
+                </div>                
+        </div> 
 
-        </n-grid>
-      
+
     </div>
 </template>
 
@@ -40,8 +28,32 @@
 </script>
 
 <style scoped>
-.profile-card{
-    background-color:transparent;
+.profile-box{
+    display:flex;
     outline:auto;
+    width:90px;
 }
+.profile-pic{
+    position:relative;
+    text-align: center;
+    color:rgb(1, 24, 8);
+    outline:auto;
+    max-width:min-content;
+}
+
+.description{
+    max-width:600px;
+    height:363px;
+    display:none;
+
+}
+
+.character-name{
+    position:absolute;    
+    left:50%;
+    transform:translate(-50%,-2000%);
+    
+}
+
+
 </style>
